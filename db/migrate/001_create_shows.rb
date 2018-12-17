433fb2e3e7cef23a86e35fa1c808114148f652c9
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-12-17 20:18:35 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -3,7 +3,6 @@ class CreateShows < ActiveRecord::Migration[5.2]
     create_table :shows do |s|
       s.string :name
       s.string :day
-      s.string :season
       s.string :network
       s.integer :rating
     end
