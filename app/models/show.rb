b5d2e73ff4f4ebd8b22154d4af9f8ff470ef55b7
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-12-17 20:36:08 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,13 +1,14 @@
 class Show < ActiveRecord::Base
   def self.highest_rating
-    self.maximum(:ratings)
+    self.maximum(:rating)
   end
 
   def self.most_popular_show
-    where(ratings: highest_rating).first
+    where(rating: highest_rating).first
   end
 
   def self.lowest_rating
+    self.minimum(:rating)
   end
 
   def self.least_popular_show
